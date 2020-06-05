f8e62dd59bfe50c2b15094a58e901bd6700d7692
Máté Cserép <mcserep@inf.elte.hu>
2020-06-05 19:39:29 +0200

    Added Processor base class to infrastructure.

---
+++ compute/Processor.h
@@ -0,0 +1,38 @@
+/*
+ * BSD 3-Clause License
+ * Copyright (c) 2019-2020, Máté Cserép
+ * All rights reserved.
+ *
+ * You may obtain a copy of the License at
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+#ifndef OLP_PROCESSOR_H
+#define OLP_PROCESSOR_H
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+namespace olp
+{
+namespace compute
+{
+template<typename PointType>
+class Processor
+{
+public:
+    virtual ~Processor() {}
+
+/**
+  * Point cloud process function to override
+  * @param input Input point cloud
+  * @return Processed output point cloud
+  */
+    virtual typename pcl::PointCloud<PointType>::Ptr process(
+        typename pcl::PointCloud<PointType>::ConstPtr input) = 0;
+};
+
+} // compute
+} // olp
+
+#endif //OLP_PROCESSOR_H
