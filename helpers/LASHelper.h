643490116ab4531f7878a5aea52658c9b0eff667
Máté Cserép <mcserep@inf.elte.hu>
2020-06-05 19:28:09 +0200

    LAS/LAZ output support.

---
+++ helpers/LASHelper.h
@@ -0,0 +1,57 @@
+/*
+ * BSD 3-Clause License
+ * Copyright (c) 2020, Máté Cserép
+ * All rights reserved.
+ *
+ * You may obtain a copy of the License at
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+#ifndef OLP_LASHELPER_H
+#define OLP_LASHELPER_H
+
+#include <string>
+#include <climits>
+
+#include <lasreader.hpp>
+#include <laswriter.hpp>
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
+namespace olp
+{
+namespace helper
+{
+LASheader readLASHeader(const std::string& filename);
+
+pcl::PointCloud<pcl::PointXYZ>::ConstPtr readLAS(
+    const std::string& filename,
+    LASheader& header,
+    unsigned long maxSize = ULONG_MAX);
+
+inline pcl::PointCloud<pcl::PointXYZ>::ConstPtr readLAS(
+    const std::string& filename,
+    unsigned long maxSize = ULONG_MAX)
+{
+    LASheader header;
+    return readLAS(filename, header, maxSize);
+}
+
+pcl::PointCloud<pcl::PointXYZ>::ConstPtr readLAS(
+    const std::string& filename,
+    LASheader& header,
+    long minX, long maxX, long minY, long maxY,
+    unsigned long maxSize = ULONG_MAX);
+
+void writeLAS(
+    const std::string& filename,
+    const LASheader& header,
+    const pcl::PointCloud<pcl::PointXYZI>::ConstPtr cloud);
+
+LASheader* createLASHeader();
+
+} // helper
+} // olp
+
+#endif //OLP_LASHELPER_H
